0247895eb2577deb0d7c4d16bf114ee787f38831
Nick Johnstone <ncwjohnstone@gmail.com>
2015-12-13 18:50:37 +1300

    Configure name and email in heroku update script

--- heroku-update.sh
+++ heroku-update.sh
@@ -8,6 +8,9 @@ cp -r ../node_modules .
 
 ./node_modules/babel-cli/bin/babel-node.js update.js
 
+git config user.email "ncwjohnstone@gmail.com"
+git config user.name "Nick Johnstone (heroku auto update)"
+
 git config credential.helper store
 
 echo https://$GIT_CREDENTIALS@github.com > ~/.git-credentials
